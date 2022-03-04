4ac51899db95518c08b45dd9d88aabbd44f9f132
Alexander Vödisch <voedisch.alexander@gmail.com>
2022-03-04 14:28:58 +0100

    fix: route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -19,7 +19,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("/idle-game/"),
+  history: createWebHashHistory(),
   routes,
 });
 
