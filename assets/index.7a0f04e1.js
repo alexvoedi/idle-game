8bbbdc12dbf8501e756404eb6ee657bdcef083f3
alexvoedi <37340862+alexvoedi@users.noreply.github.com>
2022-03-16 12:18:02 +0000

    Deploying to gh-pages from @ alexvoedi/idle-game@053578be44f26b94318072f4a4ee18e65cdc7565 🚀

---
+++ assets/index.7a0f04e1.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.71d062a6.js";import{o as r,e as o}from"./vendor.6b5c0dfd.js";const c={};function n(t,a){return r(),o("div",null,"jobs")}var f=e(c,[["render",n]]);export{f as default};
